b3e244c5004a74a8137a7a1f69a3780f4bd1aa53
daisuke <dsaito85a@gmail.com>
2019-08-21 13:33:15 +0900

    クレジットカード番号入力とトークンの返信まで実装

---
+++ app/Http/Controllers/ChargeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Cookie;
+use Illuminate\Http\Request;
+
+class ChargeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * サブスクリプションの課金を開始する
+     * Cookieの値から判断する
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function create(Request $request)
+    {
+
+        dd($request->stripeToken);
+
+        return redirect('/');
+    }
+
+    public function inputCredit()
+    {
+        return view('credit');
+    }
+}
