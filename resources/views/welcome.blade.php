d1f897ba5d5757e37add89eebec868c5bcc2a8b6
daisuke <dsaito85a@gmail.com>
2019-08-22 09:27:33 +0900

    firstViewの画像追加

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,64 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html lang="en">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+<body>
+    <img src="{{ asset('image/firstView.png') }}" alt="">
+    <div class="top">
+        <p>\\フリーランスになるためのお手伝い企画//</p>
+        <h1>むちょこ道場</h1>
+    </div>
+    <div class="about">
+        <h2>About</h2>
+        <p>むちょこ道場では、フリーランスになるための<br />お手伝いをしております。</p>
+        <p>見積もりの提出体験などもできます。</p>
+        <p>基礎力・チーム開発力・実務力をテーマとし<br />た３つの課題を通して、フリーランスのエン<br />ジニアとして、必要なスキルを体感していた<br />だくワークショップになります。</p>
+        <div>
+            <div>
+                <img src="" alt="基礎力">
+                <p>基礎力</p>
+            </div>
+            <div>
+                <img src="" alt="チーム開発力">
+                <p>チーム開発力</p>
+            </div>
+            <div>
+                <img src="" alt="実務力">
+                <p>実務力</p>
             </div>
         </div>
-    </body>
+    </div>
+    <div class="Goal">
+        <h2>目標</h2>
+        <div>
+            <div>\</div>
+            <div>フリーランスとして駆け出すために必要な<br />武器を揃えること！</div>
+            <div>/</div>
+        </div>
+    </div>
+    <div class="Apply">
+        <h2>お申し込み</h2>
+        <div>
+            <p>費用は、<span>50,000円</span>です。</p>
+            <p>お申し込みの方は、こちらから<br />決済にお進みください</p>
+            <div>
+                <a href="/apply"><img src="" alt="">お申込み</a>
+            </div>
+        </div>
+    </div>
+    <div class="Contact">
+        <div>
+            <a href="">お問い合わせ<img src="" alt=""></a>
+        </div>
+    </div>
+
+    <footer>©2019 むちょこ道場</footer>
+</body>
+
 </html>
