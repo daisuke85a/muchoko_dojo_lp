72499624f7eda2162333691b193a3b5603e1dfdd
daisuke <dsaito85a@gmail.com>
2019-08-23 21:39:48 +0900

    名前とアドレス固定でメール送信に成功

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Mail\ContactToCustomer;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
@@ -21,11 +23,16 @@ class ContactController extends Controller
      */
     public function send(Request $request)
     {
-        dd($request);
 
         // TODO::バリデーションする。必須表示する。
 
         // TODO::問い合わせした人にメールする
+        $data = [ 'email' => $request->email ];
+        Mail::send('emails.contact.customer', $data, function($message){
+    	    $message->to('daisuke@gmail.com', 'お名前')
+                        ->subject('むちょこ道場へのお問い合わせありがとうございます');
+    	});
+
 
         // TODO::むちょこ道場の管理者にメールする
 
